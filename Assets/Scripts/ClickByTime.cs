fcdc3d8ba649cb1da9fdaa7138d1a3eb37b03306
Leonardo Viana <leonardoviana.nave@gmail.com>
2020-06-24 16:58:05 -0300

    first commit

---
+++ Assets/Scripts/ClickByTime.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ClickByTime : MonoBehaviour
+{
+    Coroutine startedCorountine;
+    static Image image;
+    float currentTime = 0;
+    bool loadCircleTime = false;
+    const float duration = 3f;
+
+    void Awake()
+    {
+        if (!image)
+        {
+            image = GameObject.FindGameObjectWithTag("CircleTimer").GetComponent<Image>();
+        }
+    }
+
+    void Update()
+    {
+        if (loadCircleTime)
+        {
+            image.fillAmount = FillAmountByTime(duration);
+        }
+    }
+
+    public void PointerEnter()
+    {
+        startedCorountine = StartCoroutine(CountByClick());
+    }
+
+    public void PointerExit()
+    {
+        loadCircleTime = false;
+        currentTime = 0;
+        image.fillAmount = 0;
+        StopCoroutine(startedCorountine);
+    }
+
+    IEnumerator CountByClick()
+    {
+        loadCircleTime = true;
+        yield return new WaitForSeconds(duration);
+        ExecuteEvents.Execute<IPointerClickHandler>(this.gameObject, new PointerEventData(EventSystem.current), ExecuteEvents.pointerClickHandler);
+        PointerExit();
+    }
+
+    float FillAmountByTime(float duration)
+    {
+        currentTime += Time.deltaTime;
+        float proportion = currentTime / duration;
+        return Mathf.Clamp01(proportion);
+    }
+}
---
+++ Assets/Scripts/Waypoint.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoint : MonoBehaviour
+{
+    private static GameObject Player;
+    private static GameObject previusWaypoint;
+
+    void Awake()
+    {
+        if (!Player)
+        {
+            Player = GameObject.FindGameObjectWithTag("Player");
+        }
+    }
+
+    public void Move()
+    {
+        Player.transform.position = this.transform.position;
+
+        if (previusWaypoint)
+        {
+            previusWaypoint.SetActive(true);
+        }
+
+        previusWaypoint = this.gameObject;
+        this.gameObject.SetActive(false);
+    }
+}
